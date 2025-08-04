a9c6ada05c3ff15849b0fd9f6bc690f0090a0eac
benwu12 <54261409+benwu12@users.noreply.github.com>
2025-08-05 01:10:39 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import glob
+import os
+
+picthre_path = glob.glob('/Users/ben/Pictures/*')
+floder_path = [
+    path for path in picthre_path
+    if os.path.isdir(path) and "已備份" not in os.path.basename(path)
+]
+for folder in floder_path:
+    if folder.endswith(".photoslibrary"):
+        print(f"跳過 macOS 系統保護資料夾：{folder}")
+        continue
+
+    base = os.path.basename(folder)
+    parent = os.path.dirname(folder)
+    new_name = base + "＿已備份"
+    new_path = os.path.join(parent, new_name)
+    os.rename(folder, new_path)
+    print(f"已重新命名: {folder}")
